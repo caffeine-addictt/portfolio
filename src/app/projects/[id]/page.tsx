16e47b89b9c708d5edd04812e7c90cef7416dec4
AlexNg <taboez4life@gmail.com>
2023-12-03 23:25:27 +0800

    Project setup

---
+++ src/app/projects/[id]/page.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const ProjectFocusedPage = (s: PageParameter) => {
+  return (
+    <>
+      ProjectFocusedPage
+    </>
+  );
+};
+
+export default ProjectFocusedPage
+
+
+interface PageParameter {
+  params: { id: string },
+  searchParams: { }
+}
