22596198e858784fb727d25990e8c911045f9e9a
AlexNg <taboez4life@gmail.com>
2024-01-10 22:54:44 +0800

    + tailwind class merger

---
+++ src/utils/tailwind.ts
@@ -0,0 +1,16 @@
+import clsx, { ClassValue } from 'clsx'
+import { twMerge } from 'tailwind-merge'
+
+
+/**
+ * Classess with a higher index overwrite classes with a lower index
+ * * `cn( 'bg-red', 'bg-black' ) -> 'bg-black'`
+ * 
+ * @param classes - Classes to merge
+ * @returns Merged classes
+ */
+export const cn = (...classes: ClassValue[]) => {
+  return twMerge(clsx(...classes))
+}
+
+
