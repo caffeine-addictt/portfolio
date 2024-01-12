84ff619cf179d0bb95d8094cf0f39fecb9e8f16f
AlexNg <taboez4life@gmail.com>
2024-01-12 12:12:13 +0800

    Mounting logic

--- src/components/theme-provider.tsx
+++ src/components/theme-provider.tsx
@@ -1,19 +1,23 @@
 'use client'
 
 import * as React from 'react'
+import { useState, useEffect } from 'react'
+import { useTheme, ThemeProvider as NextThemeProvider } from 'next-themes'
+import { type ThemeProviderProps } from 'next-themes/dist/types'
 
 import { MoonIcon, SunIcon } from '@radix-ui/react-icons'
 import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
-  DropdownMenuTrigger,
+  DropdownMenuTrigger
 } from '@components/ui/dropdown-menu'
 import { Button } from '@components/ui/button'
 
-import { useTheme } from 'next-themes'
-import { ThemeProvider as NextThemeProvider } from 'next-themes'
-import { type ThemeProviderProps } from 'next-themes/dist/types'
+import { titleCase } from '@utils/strings'
+
+
+
 
 /** Wraps children in root layout to enable themes */
 export const ThemeProvider = ({ children, ...props }: ThemeProviderProps) => {
@@ -24,30 +28,58 @@ export const ThemeProvider = ({ children, ...props }: ThemeProviderProps) => {
 
 
 /** Button to enable switching themes */
-export function ModeToggle() {
-  const { setTheme } = useTheme()
+export const ModeToggle = () => {
+  const [mounted, setMounted] = useState(false)
+  const { theme, setTheme } = useTheme()
+
+  // Handle mounting logic
+  useEffect(() => setMounted(true), [])
 
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button variant='outline' size='icon'>
-          <SunIcon className='h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0' />
-          <MoonIcon className='absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100' />
-          <span className='sr-only'>Toggle theme</span>
-        </Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent align='end'>
-        <DropdownMenuItem onClick={() => setTheme('light')}>
-          Light
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme('dark')}>
-          Dark
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme('system')}>
-          System
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
+    <>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button variant='outline' size='icon'>
+            <SunIcon className='h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0' />
+            <MoonIcon className='absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100' />
+            <span className='sr-only'>Toggle theme</span>
+          </Button>
+        </DropdownMenuTrigger>
+
+
+        {/* Options */}
+        <DropdownMenuContent align='end'>
+          <DropdownItem name='light' theme={theme} mounted={mounted} setTheme={setTheme} />
+          <DropdownItem name='dark' theme={theme} mounted={mounted} setTheme={setTheme} />
+          <DropdownItem name='system' theme={theme} mounted={mounted} setTheme={setTheme} />
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </>
   )
 }
 
+
+interface DropdownItemProps {
+  name: string
+  theme?: string
+  mounted: boolean
+  setTheme: (theme: string) => void
+}
+/**
+ * Dropdown menu item
+ * @param {DropdownItemProps} props
+ * @param props.name - Name of the theme
+ * @param props.theme - Current theme
+ * @param props.mounted - Whether the component is mounted
+ * @param props.setTheme - Function to set the theme
+ */
+const DropdownItem = ({ name, theme, mounted, setTheme }: DropdownItemProps) => {
+  return (
+    <DropdownMenuItem
+      onClick={() => setTheme(name)}
+      disabled={mounted && theme === name}
+    >
+      {titleCase(name)}
+    </DropdownMenuItem>
+  )
+}
