7983cedd53b40260a68af48cc548f79c0f3fe115
AlexNg <taboez4life@gmail.com>
2024-01-13 12:16:45 +0800

    + classname option

--- src/components/theme-provider.tsx
+++ src/components/theme-provider.tsx
@@ -28,14 +28,14 @@ export const ThemeProvider = ({ children, ...props }: ThemeProviderProps) => {
 
 
 /** Button to enable switching themes */
-export const ModeToggle = () => {
+export const ModeToggle = ({ className }: { className?: string }) => {
   const { theme, setTheme } = useTheme()
 
   return (
     <DropdownMenu>
-      <TooltipWrapper text='Switch theme'>
+      <TooltipWrapper text='Switch theme' asChild>
         <DropdownMenuTrigger asChild>
-          <Button variant='outline' size='icon'>
+          <Button variant='outline' size='icon' className={className}>
             <SunIcon className='h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0' />
             <MoonIcon className='absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100' />
             <span className='sr-only'>Toggle theme</span>
