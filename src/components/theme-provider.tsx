e671a8e4707d0670278f9eb356c1d8afc0c8cd40
AlexNg <taboez4life@gmail.com>
2024-01-13 11:30:04 +0800

    Wrap theme switcher in tooltip

--- src/components/theme-provider.tsx
+++ src/components/theme-provider.tsx
@@ -12,6 +12,7 @@ import {
   DropdownMenuTrigger
 } from '@components/ui/dropdown-menu'
 import { Button } from '@components/ui/button'
+import { TooltipWrapper } from '@components/ui/tooltip'
 
 import { titleCase } from '@utils/strings'
 
@@ -32,13 +33,15 @@ export const ModeToggle = () => {
 
   return (
     <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button variant='outline' size='icon'>
-          <SunIcon className='h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0' />
-          <MoonIcon className='absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100' />
-          <span className='sr-only'>Toggle theme</span>
-        </Button>
-      </DropdownMenuTrigger>
+      <TooltipWrapper text='Switch theme'>
+        <DropdownMenuTrigger asChild>
+          <Button variant='outline' size='icon'>
+            <SunIcon className='h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0' />
+            <MoonIcon className='absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100' />
+            <span className='sr-only'>Toggle theme</span>
+          </Button>
+        </DropdownMenuTrigger>
+      </TooltipWrapper>
 
 
       {/* Options */}
