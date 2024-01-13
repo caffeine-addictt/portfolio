07cf52d50ae6be7f325773c5dcb913769a2c826a
AlexNg <taboez4life@gmail.com>
2024-01-14 01:33:40 +0800

    + sitemap.xml

---
+++ src/app/sitemap.ts
@@ -0,0 +1,16 @@
+import { MetadataRoute } from 'next'
+
+export default async (): Promise<MetadataRoute.Sitemap> => {
+  // const projects = await sanityClient.fetch(`*[_type == "project"]`)
+  // const blog = await sanityClient.fetch(`*[_type == "blog"]`)
+
+  const fetchedData: MetadataRoute.Sitemap = []
+
+  return [
+    { url: `${process.env.NEXT_PUBLIC_BASE_URL}` },
+    { url: `${process.env.NEXT_PUBLIC_BASE_URL}/about` },
+    { url: `${process.env.NEXT_PUBLIC_BASE_URL}/contact` },
+    { url: `${process.env.NEXT_PUBLIC_BASE_URL}/projects` },
+    ...fetchedData
+  ]
+}
