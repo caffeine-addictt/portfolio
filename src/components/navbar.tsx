2a7d05830fcc1fab74ac43be9f3eb90b0e72ca4f
AlexNg <taboez4life@gmail.com>
2024-01-13 13:00:09 +0800

    - Remove unused import

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -2,7 +2,6 @@
 
 import * as React from 'react'
 import Link from 'next/link'
-import Image from 'next/image'
 
 import { cn } from '@utils/tailwind'
 import { buttonVariants } from '@components/ui/button'
