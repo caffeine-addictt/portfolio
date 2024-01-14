35291d8abd666250299c9bb32d731185da18a92c
AlexNg <taboez4life@gmail.com>
2024-01-14 16:19:20 +0800

    + Project query helpers

--- src/lib/sanity/client.ts
+++ src/lib/sanity/client.ts
@@ -1,6 +1,11 @@
+import { cache } from 'react'
 import { createClient } from 'next-sanity'
+import imageUrlBuilder from '@sanity/image-url'
+
+import { ProjectItem } from './schema'
 
 
+/** Sanity client */
 export const client = createClient({
   apiVersion: process.env.NEXT_PUBLIC_API_VERSION,
   dataset: process.env.NEXT_PUBLIC_DATASET,
@@ -9,6 +14,46 @@ export const client = createClient({
 })
 
 
+/** Image builder */
+const builder = imageUrlBuilder(client)
+export const urlFor = (source: any) => builder.image(source)
+
+
 
 
 // Fetch querys
+export const getAllProjects = cache(async (offset: number = 0, limit: number = 10): Promise<ProjectItem[]> => {
+  const data = await client.fetch(`
+    *[_type == "projects"]{
+      "slug": slug.current,
+      title,
+      "description": {
+        "short": description.short,
+        "long": description.long
+      },
+      links,
+      technologies,
+      timeframe,
+      ...
+    }[${offset}...${limit}]
+  `)
+  return data
+})
+
+export const getProject = cache(async (slug: string): Promise<ProjectItem> => {
+  const data = await client.fetch(`
+    *[_type == "projects" && slug.current == "${slug.toLowerCase()}"]{
+      "slug": slug.current,
+      title,
+      "description": {
+        "short": description.short,
+        "long": description.long
+      },
+      links,
+      technologies,
+      timeframe,
+      ...
+    }[0]
+  `)
+  return data
+})
