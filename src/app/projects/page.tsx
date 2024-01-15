18298506909608bf9d20881a59b11c84463f5e00
AlexNg <taboez4life@gmail.com>
2024-01-15 18:20:57 +0800

    + project list page

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,7 +1,11 @@
 import * as React from 'react'
-
 import { Metadata } from 'next'
-import { getAllProjects } from '@lib/sanity/client'
+import { getAllSkills, getAllProjects } from '@lib/sanity/client'
+
+import SearchUI from './search'
+import { ProjectCards } from './projectcard'
+
+
 
 
 export const metadata: Metadata = {
@@ -10,15 +14,26 @@ export const metadata: Metadata = {
 }
 
 
-const ProjectsListPage = async () => {
+interface PageParamProps { searchParams?: { query?: string; page?: string; tech?: string[] | string } }
+const ProjectsListPage = async ({ searchParams }: PageParamProps) => {
   const data = await getAllProjects()
+  const skills = await getAllSkills()
+
   return (
-    <div>
-      {data.map((project, key) => (
-        <div key={key}>
-          <h1>{project.title}</h1>
-        </div>
-      ))}
+    <div className='flex min-h-screen min-w-full max-w-full flex-col items-center' style={{ minHeight: 'calc(100vh - 64px)' }}>
+
+      {/* Filtering */}
+      <SearchUI
+        skills={skills}
+        searchParams={{
+          ...searchParams,
+          tech: !!searchParams?.tech ? Array.from(Array.isArray(searchParams?.tech) ? searchParams?.tech : [searchParams?.tech]) : undefined
+        }}
+      />
+
+      <div className='flex flex-wrap gap-2 mb-4 w-[80%] max-sm:w-[97.5%] self-center justify-center'>
+        <ProjectCards data={data} />
+      </div>
     </div>
   )
 }
