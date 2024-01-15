a714c1cfc947929b0ca48f35ff6d089946e47ce1
AlexNg <taboez4life@gmail.com>
2024-01-15 18:04:10 +0800

    Bugfix: technologies rendered as reference

--- src/lib/sanity/client.ts
+++ src/lib/sanity/client.ts
@@ -2,11 +2,11 @@ import { cache } from 'react'
 import { createClient } from 'next-sanity'
 import imageUrlBuilder from '@sanity/image-url'
 
-import { ProjectItem } from './schema'
+import { ProjectItem, SkillsItem } from './schema'
 
 
 /** Sanity client */
-export const client = createClient({
+const client = createClient({
   apiVersion: process.env.NEXT_PUBLIC_API_VERSION,
   dataset: process.env.NEXT_PUBLIC_DATASET,
   projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
@@ -16,13 +16,13 @@ export const client = createClient({
 
 /** Image builder */
 const builder = imageUrlBuilder(client)
-export const urlFor = (source: any) => builder.image(source)
+const urlFor = (source: any) => builder.image(source)
 
 
 
 
 // Fetch querys
-export const getAllProjects = (async (offset: number = 0, limit: number = 10): Promise<ProjectItem[]> => {
+const getAllProjects = (async (offset: number = 0, limit: number = 10): Promise<ProjectItem[]> => {
   const data = await client.fetch(`
     *[_type == "projects"]{
       ...,
@@ -33,14 +33,19 @@ export const getAllProjects = (async (offset: number = 0, limit: number = 10): P
         "long": description.long
       },
       links,
-      technologies,
+      technologies[]->{
+        ...,
+        name,
+        href,
+        start_time
+      },
       timeframe
     }[${offset}...${limit}]
   `)
   return data
 })
 
-export const getProject = (async (slug: string): Promise<ProjectItem> => {
+const getProject = (async (slug: string): Promise<ProjectItem> => {
   const data = await client.fetch(`
     *[_type == "projects" && slug.current == "${slug.toLowerCase()}"]{
       ...,
@@ -51,9 +56,25 @@ export const getProject = (async (slug: string): Promise<ProjectItem> => {
         "long": description.long
       },
       links,
-      technologies,
+      technologies[]->{
+        ...,
+        name,
+        href,
+        start_time
+      },
       timeframe
     }[0]
   `)
   return data
 })
+
+
+
+
+
+export {
+  client,
+  urlFor,
+  getProject,
+  getAllProjects
+}
