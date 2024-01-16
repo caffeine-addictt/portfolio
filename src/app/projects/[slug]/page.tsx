f176fab307d7864c51dc49808a69bd4553ba829d
AlexNg <taboez4life@gmail.com>
2024-01-16 08:59:04 +0800

    + Page layout

--- src/app/projects/[slug]/page.tsx
+++ src/app/projects/[slug]/page.tsx
@@ -3,28 +3,39 @@ import { Suspense } from 'react'
 
 import Image from 'next/image'
 import { Metadata } from 'next'
+import { PortableText } from '@portabletext/react'
+import { escapeQueryString } from '@utils/strings'
+import { urlFor, queryProjects } from '@lib/sanity/client'
 
 import { Skeleton } from '@components/ui/skeleton'
-import { urlFor, getProject, getAllProjects } from '@lib/sanity/client'
+import { Separator } from '@components/ui/separator'
+import { InternalLink } from '@components/ui/button'
+import { TooltipWrapper } from '@components/ui/tooltip'
+import BackToTopButton from '@components/totop-button'
 
 
 
 
 // SEO
 export const generateStaticParams = async () => {
-  const projects = await getAllProjects(5)
-  return projects.map((project) => project.slug)
+  const projects = await queryProjects({ queryLength: 50, orderByRecency: true, lookingFor: ['"slug": slug.current'] })
+  return projects.map((project) => ({ slug: project.slug }))
 }
 
 export const generateMetadata = async ({ params: { slug } }: { params: { slug: string } }): Promise<Metadata> => {
-  const data = await getProject(slug)
+  const fetched = (await queryProjects({ slug: escapeQueryString(slug), queryLength: 1 }))
+
+  if (!!!fetched.length) {
+    return {
+      title: 'Looks like this project doesn\'t exist!',
+      description: `Oops! Looks like this project doesn't exist! Check out my other projects instead at ${process.env.NEXT_PUBLIC_BASE_URL}/projects!`
+    }
+  }
+
+  const data = fetched[0]
   return {
     title: data.title,
-    description: data.description.long.join('\n'),
-    openGraph: {
-      title: data.title,
-      description: data.description.long.join('\n')
-    }
+    description: data.description.short
   }
 }
 
@@ -33,29 +44,74 @@ export const generateMetadata = async ({ params: { slug } }: { params: { slug: s
 
 // Render Logic
 const ProjectPage = async ({ params: { slug } }: { params: { slug: string } }) => {
-  const data = await getProject(slug)
+  const fetched = (await queryProjects({ slug: escapeQueryString(slug), queryLength: 1 }))
+
+  if (!!!fetched.length) {
+    return (
+      <div className='flex min-h-screen min-w-full max-w-full flex-col items-center' style={{ minHeight: 'calc(100vh - 64px)' }}>
+        <div className='mt-8 flex w-[80%] flex-col items-center max-sm:w-[97.5%]'>
+          <h1 className='mx-auto w-fit text-5xl font-bold sm:w-[50%]'>
+            <span className='block text-center text-base uppercase tracking-wide text-accent-light dark:text-accent-dark'>Alex Ng - Project</span>
+            <span className='mt-2 block text-center text-5xl leading-8 tracking-tight sm:text-6xl'>Looks like this project doesn't exist!</span>
+          </h1>
+
+          <TooltipWrapper text='Check out my other projects!' asChild>
+            <InternalLink href={`${process.env.NEXT_PUBLIC_BASE_URL}/projects`} variant='ghost' className='mt-8 text-base font-light'>
+              Check out my other projects instead!
+            </InternalLink>
+          </TooltipWrapper>
+        </div>
+      </div>
+    )
+  }
+
+  const data = fetched[0]
+  const newStart = new Date(data.timeframe.start)
+  const newEnd = data.timeframe.end && new Date(data.timeframe.end)
 
   return (
     <div className='flex min-h-screen min-w-full max-w-full flex-col items-center' style={{ minHeight: 'calc(100vh - 64px)' }}>
-      <div className='w-[80%] max-sm:w-[97.5%] mt-8 flex flex-col items-center'>
+      <div className='mt-8 flex w-[80%] flex-col items-center max-sm:w-[97.5%]'>
 
-        {/* Title */}
-        <h1 className='text-5xl font-bold'>{data.title}</h1>
+        {/* Header */}
+        <h1 className='mx-auto w-fit text-5xl font-bold sm:w-[50%]'>
+          <span className='block text-center text-base uppercase tracking-wide text-accent-light dark:text-accent-dark'>Alex Ng - Project</span>
+          <span className='mt-2 block text-center text-5xl leading-8 tracking-tight sm:text-6xl'>{data.title}</span>
+        </h1>
+
+        {/* Time */}
+        <p className='mt-2 text-base font-light'>
+          {newStart.getUTCMonth()+1}/{newStart.getUTCFullYear()}
+          -
+          {newEnd ? `${newEnd.getUTCMonth()+1}/${newEnd.getUTCFullYear()}` : 'Present'}
+        </p>
+
+        {/* Separator */}
+        <Separator className='my-4 w-[30%]' />
+
+        {/* Short description */}
+        <p className='text-lg'>
+          {data.description.short}
+        </p>
+
 
         {/* Image */}
-        <div className='mt-2 mb-8 w-auto h-96 aspect-video flex items-center'>
+        <div className='mt-8 flex aspect-video h-96 max-h-96 w-auto items-center overflow-hidden rounded-lg border'>
           <Suspense fallback={<HeaderImageSkeleton />}>
             <HeaderImage imageObj={data.images.image} />
           </Suspense>
         </div>
 
 
-        {/* Description */}
-        <p className='text-lg'>
-          {data.description.long.join('\n')}
-        </p>
-
+        {/* Content */}
+        <div className='prose prose-xl my-8 w-[80%] text-text-light dark:prose-invert dark:text-text-dark max-sm:w-[97.5%]'>
+          <PortableText value={data.description.long} />
+        </div>
       </div>
+
+
+      {/* Floating back-to-top button */}
+      <BackToTopButton />
     </div>
   )
 }
@@ -65,7 +121,7 @@ export default ProjectPage
 
 
 // Image
-const HeaderImageSkeleton = () => <Skeleton className='h-full w-full'/>
+const HeaderImageSkeleton = () => <Skeleton className='h-96 w-screen'/>
 const HeaderImage = async ({ imageObj }: { imageObj: any }) => {
   if (imageObj) {
     try {
@@ -75,8 +131,9 @@ const HeaderImage = async ({ imageObj }: { imageObj: any }) => {
           alt='Project Image'
           width={1600}
           height={1200}
-          className='w-full h-full'
+          className='h-full w-full'
           loading='eager'
+          priority
         />
       )
     }
@@ -89,8 +146,9 @@ const HeaderImage = async ({ imageObj }: { imageObj: any }) => {
       alt='Project Image'
       width={1600}
       height={1200}
-      className='w-full h-full'
+      className='h-full w-full'
       loading='eager'
+      priority
     />
   )
 }
