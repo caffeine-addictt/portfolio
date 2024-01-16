867632f8bff168a2415212731a950431a5687ecc
AlexNg <taboez4life@gmail.com>
2024-01-16 21:01:02 +0800

    + Default rendered image

--- src/app/projects/[slug]/opengraph.tsx
+++ src/app/projects/[slug]/opengraph.tsx
@@ -2,6 +2,7 @@
 /* eslint-disable react/no-unknown-property */
 
 import * as React from 'react'
+import Image from 'next/image'
 import { ImageResponse } from 'next/og'
 
 import { escapeQueryString } from '@utils/strings'
@@ -18,17 +19,26 @@ export const size = {
 
 
 const og = async ({ params: { slug } }: { params: { slug: string } }) => {
-  const fetched = (await queryProjects({ slug: escapeQueryString(slug), queryLength: 1, lookingFor: ['images', 'title'] }))[0]
+  const fetched = (await queryProjects({ slug: escapeQueryString(slug), queryLength: 1, lookingFor: ['images', 'title', 'description'] }))[0]
   return new ImageResponse(
     <div tw='relative flex w-full h-full flex items-center justify-center'>
       {/* Background */}
       <div tw='absolute flex inset-0'>
-        <img
-          src={urlFor(fetched.images.image).url()}
-          alt={fetched.title}
-          width={size.width}
-          height={size.height}
-        />
+        {fetched.images?.image ? (
+          <Image
+            src={urlFor(fetched.images?.image).url()}
+            alt={fetched.title}
+            width={size.width}
+            height={size.height}
+          />
+        ) : (
+          <Image
+            src={`${process.env.NEXT_PUBLIC_BASE_URL}/images/siteImage.png`}
+            alt={fetched.title}
+            width={size.width}
+            height={size.height}
+          />
+        )}
 
         {/* Overlay */}
         <div tw='absolute inset-0 bg-black bg-opacity-50 z-10' />
