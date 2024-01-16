77ffb084f612f5f7679c81116473bed6c4957ff0
AlexNg <taboez4life@gmail.com>
2024-01-16 08:58:02 +0800

    + base url to env types

--- src/types/environment.d.ts
+++ src/types/environment.d.ts
@@ -1,6 +1,8 @@
 declare global {
   namespace NodeJS {
     interface ProcessEnv {
+      NEXT_PUBLIC_BASE_URL: string
+      
       NEXT_PUBLIC_DATASET: string
       NEXT_PUBLIC_PROJECT_ID: string
       NEXT_PUBLIC_API_VERSION: string
