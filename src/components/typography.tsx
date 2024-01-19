1b9c57d809c6c8ad81ca58797dc79a498de1e8e8
AlexNg <taboez4life@gmail.com>
2024-01-19 17:03:14 +0800

    + Added image rendering support

--- src/components/typography.tsx
+++ src/components/typography.tsx
@@ -1,8 +1,13 @@
-'use client'
-
 import * as React from 'react'
+import { Suspense } from 'react'
+import Image from 'next/image'
+
 import { cn } from '@utils/tailwind'
+import { urlFor, getImageDimensions } from '@lib/sanity/client'
 import { PortableText } from '@portabletext/react'
+
+import { Skeleton } from '@components/ui/skeleton'
+import { AspectRatio } from '@components/ui/aspect-ratio'
 
 
 /**
@@ -13,7 +18,37 @@ const EnforceTypographyStyling = React.forwardRef<
   React.ComponentRef<'article'>,
   React.ComponentPropsWithoutRef<typeof PortableText> & { className?: string }
 >(({ className, ...props }, ref) => {
-  const preRender = <PortableText {...props} />
+  const preRender = (
+    <PortableText
+      {...props}
+      components={{
+        ...props.components,
+        types: {
+          image: ({ value }) => {
+            const { width, height } = getImageDimensions(value)
+            if (!width || !height) return null
+
+            return (
+              <div className='my-8 flex h-96 max-h-96 w-auto items-center'>
+                <AspectRatio ratio={width / height}>
+                  <Suspense fallback={<Skeleton className='h-full w-full' />}>
+                    <Image
+                      src={urlFor(value).url()}
+                      alt={value.alt || ''}
+                      width={width}
+                      height={height}
+                      loading='lazy'
+                      className='h-full w-full rounded-lg object-cover'
+                    />
+                  </Suspense>
+                </AspectRatio>
+              </div>
+            )
+          },
+        }
+      }}
+    />
+  )
 
   return (
     <>
