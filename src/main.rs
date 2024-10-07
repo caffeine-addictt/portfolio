73562c4583cee47ce1e7a1ca75aa777c116df4b9
AlexNg <contact@ngjx.org>
2024-10-08 01:10:06 +0800

    feat: Add error and ratelimiting middleware
    Signed-off-by: AlexNg <contact@ngjx.org>

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,15 @@
+use std::time::Duration;
+
 use anyhow::Result;
+use axum::{
+    error_handling::HandleErrorLayer,
+    extract::MatchedPath,
+    http::{HeaderMap, Request, Response, StatusCode},
+    BoxError, Extension,
+};
+use tower::{buffer::BufferLayer, limit::RateLimitLayer, ServiceBuilder};
+use tower_http::{classify::ServerErrorsFailureClass, trace::TraceLayer};
+use tracing::{debug, error, info, info_span, Span};
 
 mod routes;
 
@@ -15,6 +26,51 @@ async fn main() -> Result<(), std::io::Error> {
         listener,
         routes::get_routes()
             .layer(Extension(tera))
+            .layer(
+                TraceLayer::new_for_http()
+                    .make_span_with(|request: &Request<_>| {
+                        let matched_path = request
+                            .extensions()
+                            .get::<MatchedPath>()
+                            .map(MatchedPath::as_str);
+
+                        info_span!(
+                            "http_request",
+                            method = ?request.method(),
+                            matched_path,
+                        )
+                    })
+                    .on_request(|_request: &Request<_>, _span: &Span| {
+                        info!("request started");
+                    })
+                    .on_response(
+                        |_response: &Response<_>, _latency: Duration, _span: &Span| {
+                            info!("request complete");
+                        },
+                    )
+                    .on_eos(
+                        |_trailers: Option<&HeaderMap>,
+                         _stream_duration: Duration,
+                         _span: &Span| { debug!("stream closed") },
+                    )
+                    .on_failure(
+                        |_error: ServerErrorsFailureClass, _latency: Duration, _span: &Span| {
+                            let e = _error.to_string();
+                            error!(e, "request failed");
+                        },
+                    ),
+            )
+            .layer(
+                ServiceBuilder::new()
+                    .layer(HandleErrorLayer::new(|err: BoxError| async move {
+                        (
+                            StatusCode::INTERNAL_SERVER_ERROR,
+                            format!("Unhandled error: {}", err),
+                        )
+                    }))
+                    .layer(BufferLayer::new(1024))
+                    .layer(RateLimitLayer::new(5, Duration::from_secs(1))),
+            ),
     )
     .await
 }
