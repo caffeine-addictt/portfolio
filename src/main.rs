def59a5301c02c54185a9a483110867ebaa2cc96
AlexNg <contact@ngjx.org>
2024-10-08 01:06:14 +0800

    feat: Add empty templates
    Signed-off-by: AlexNg <contact@ngjx.org>

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,14 @@ async fn main() -> Result<(), std::io::Error> {
     // logging
     tracing_subscriber::fmt::init();
 
+    // templates
+    let tera = tera::Tera::new("templates/**/*").expect("creating tera templates to not error");
+
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await?;
-    axum::serve(listener, routes::get_routes()).await
+    axum::serve(
+        listener,
+        routes::get_routes()
+            .layer(Extension(tera))
+    )
+    .await
 }
