c84c8437e051f8830daae55a42ff8e9c6aaaa90d
AlexNg <contact@ngjx.org>
2024-10-08 01:08:48 +0800

    feat: Add root path
    Signed-off-by: AlexNg <contact@ngjx.org>

--- src/routes.rs
+++ src/routes.rs
@@ -1,6 +1,14 @@
-use axum::Router;
+use axum::{response::Html, routing::get, Extension, Router};
 use tower_http::services::ServeDir;
+use tracing::instrument;
 
 pub fn get_routes() -> Router {
-    Router::new().nest_service("/static", ServeDir::new("public"))
+    Router::new()
+        .route("/", get(root_path))
+        .nest_service("/static", ServeDir::new("public"))
+}
+
+#[instrument]
+async fn root_path(Extension(tera): Extension<tera::Tera>) -> Html<String> {
+    Html(tera.render("index.html", &tera::Context::new()).unwrap())
 }
