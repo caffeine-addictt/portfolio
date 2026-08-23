815418639299fb905feb9e1e6d53e73a3639b186
AlexNg <contact@ngjx.org>
2026-08-23 17:14:49 +0800

    feat: Add resume download page
    Signed-off-by: AlexNg <contact@ngjx.org>

--- src/routes.rs
+++ src/routes.rs
@@ -1,10 +1,16 @@
-use axum::{response::Html, routing::get, Extension, Router};
+use axum::{
+    response::{Html, Redirect},
+    routing::get,
+    Extension, Router,
+};
+use chrono::{Datelike, Local, NaiveDate};
 use tower_http::services::ServeDir;
 use tracing::instrument;
 
 pub fn get_routes() -> Router {
     Router::new()
         .route("/", get(root_path))
+        .route("/resume", get(async || Redirect::permanent("https://raw.githubusercontent.com/caffeine-addictt/caffeine-addictt/refs/heads/main/media/ng_jun_xiang_resume.pdf")))
         .nest_service("/static", ServeDir::new("public"))
 }
 
