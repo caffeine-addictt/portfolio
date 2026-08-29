6a8fff411f2e98fca38cf2ac0c2b4cebc0eb0779
AlexNg <contact@ngjx.org>
2026-08-29 18:34:08 +0800

    feat: Add education & work stuffs
    Signed-off-by: AlexNg <contact@ngjx.org>

---
+++ src/routes/index.rs
@@ -0,0 +1,59 @@
+use axum::{response::Html, Extension};
+use chrono::{Datelike, Local, NaiveDate};
+use serde::{Deserialize, Serialize};
+use std::sync::LazyLock;
+use tracing::instrument;
+
+use crate::routes::get_tera_ctx;
+
+static WORK: LazyLock<Vec<TimelineGroup>> = LazyLock::new(|| {
+    serde_json::from_str(include_str!("../../public/work.json"))
+        .expect("failed to parse public/work.json")
+});
+static EDU: LazyLock<Vec<TimelineGroup>> = LazyLock::new(|| {
+    serde_json::from_str(include_str!("../../public/education.json"))
+        .expect("failed to parse public/education.json")
+});
+
+#[instrument]
+pub async fn root_path(Extension(tera): Extension<tera::Tera>) -> Html<String> {
+    let mut ctx = get_tera_ctx();
+
+    let today = Local::now().date_naive();
+    let birth = NaiveDate::from_ymd_opt(2006, 9, 30).unwrap();
+    let age = today.year()
+        - birth.year()
+        - ((today.month(), today.day()) < (birth.month(), birth.day())) as i32;
+    ctx.insert("age", &age);
+
+    ctx.insert("work", &*WORK);
+    ctx.insert("edu", &*EDU);
+
+    Html(tera.render("index.html", &ctx).unwrap())
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+struct TimelineGroup {
+    pub logo: String,
+    pub site_link: String,
+
+    pub name_short: String,
+    pub name_long: String,
+
+    pub items: Vec<TimelineItem>,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+struct TimelineItem {
+    pub sub_title: String,
+    pub duration_flavor: String,
+    pub description: Vec<String>,
+
+    pub links: Vec<TimelineItemLink>,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+struct TimelineItemLink {
+    pub name: String,
+    pub link: String,
+}
---
+++ src/routes/mod.rs
@@ -0,0 +1,21 @@
+use axum::{response::Redirect, routing::get, Router};
+use chrono::{Datelike, Local};
+use tower_http::services::ServeDir;
+
+mod index;
+
+pub fn get_tera_ctx() -> tera::Context {
+    let mut ctx = tera::Context::new();
+    ctx.insert("year", &Local::now().year());
+    ctx.insert("site_name", "ngjx.org");
+    ctx
+}
+
+pub fn get_routes() -> Router {
+    Router::new()
+        .route("/", get(index::root_path))
+        .route("/resume", get(async || Redirect::permanent("https://raw.githubusercontent.com/caffeine-addictt/caffeine-addictt/refs/heads/main/media/ng_jun_xiang_resume.pdf")))
+        .nest_service("/img", ServeDir::new("public/images"))
+        .nest_service("/css", ServeDir::new("public/css"))
+        .fallback_service(ServeDir::new("public/root"))
+}
