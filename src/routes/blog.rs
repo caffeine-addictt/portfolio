dee75ae869b8b26fddb201fa201239f358585c36
AlexNg <contact@ngjx.org>
2026-08-30 21:00:04 +0800

    feat: impl fetching posts
    Signed-off-by: AlexNg <contact@ngjx.org>

--- src/routes/blog.rs
+++ src/routes/blog.rs
@@ -5,15 +5,18 @@ use serde::{Deserialize, Serialize};
 
 use crate::routes::{get_tera_ctx, Result};
 
-#[derive(Debug, Clone, Serialize)]
+#[derive(Debug, Clone, Serialize, Deserialize)]
 struct BlogPost {
     title: String,
     sub_title: String,
     body: String,
     lcp_img_url: Option<String>,
 
+    /// serialized and stored as a [String] w/ [serde] from [Vec<String>]
     tags: Vec<String>,
+    /// serialized and stored as a [String] w/ [serde] from [chrono::NaiveDate]
     published_at: Option<String>,
+    /// serialized and stored as a [String] w/ [serde] from [PostStatus]
     status: String,
     view_count: i64,
 }
@@ -38,6 +41,7 @@ pub async fn recent_posts(
         SELECT
             title,
             sub_title,
+            body,
             tags,
             published_at,
             view_count
@@ -55,23 +59,23 @@ pub async fn recent_posts(
         posts.push(BlogPost {
             title: row.get::<String>(0)?,
             sub_title: row.get::<String>(1)?,
-            body: "".into(),
+            body: row.get::<String>(2)?,
             lcp_img_url: None,
-            tags: serde_json::from_str(&row.get::<String>(2)?)?,
-            published_at: row.get::<Option<String>>(3)?,
+            tags: serde_json::from_str(&row.get::<String>(3)?)?,
+            published_at: row
+                .get::<Option<String>>(4)?
+                .map(|s| {
+                    chrono::NaiveDate::parse_from_str(&s, "%Y-%m-%d")
+                        .map(|date| date.format("%b %Y").to_string())
+                })
+                .transpose()?,
             status: "published".into(),
-            view_count: row.get::<i64>(4)?,
+            view_count: row.get::<i64>(5)?,
         });
     }
 
     let mut ctx = get_tera_ctx();
     ctx.insert("posts", &posts);
+    ctx.insert("not_found_msg", "Stay tuned for some posts! :p");
     Ok(Html(tera.render("components/post.html", &ctx)?))
 }
-
-// #[instrument]
-// pub async fn projects(Extension(tera): Extension<tera::Tera>) -> Html<String> {
-//     let mut ctx = get_tera_ctx();
-//     ctx.insert("projects", &*PROJECTS);
-//     Html(tera.render("projects.html", &ctx).unwrap())
-// }
